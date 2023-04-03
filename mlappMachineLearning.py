b1b3d434fde2f4bf65094bff7b37e8481099c525
Deepak Kumar <66191666+deepakshf@users.noreply.github.com>
2023-04-03 11:26:45 +0530

    Add files via upload

---
+++ mlappMachineLearning.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 10:25:47 2023
+
+@author: kdeep
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas_profiling as pf
+from streamlit_pandas_profiling import st_profile_report
+from pycaret.classification import setup, compare_models, pull, save_model, ClassificationExperiment
+from pycaret.regression import setup, compare_models, pull, save_model, RegressionExperiment 
+
+
+import os
+
+st.title('Machine Learning App using Pycaret')
+
+if os.path.exists("sourcev.csv"):
+    data = pd.read_csv('sourcev.csv',index_col=None)
+
+with st.sidebar:
+    st.title('Welcome to Pycaret')
+    st.write('This application is made up for learning machine models.')
+    st.write('Choose your parameters here work with parameters')
+    choose = st.radio('Choose you options',['Dataset', 'EDA','Training','Download'])
+    
+    
+    
+if choose == 'Dataset':
+    st.write('Upload your Dataset')
+    upload = st.file_uploader('Upload here')
+    if upload:
+        data = pd.read_csv(upload,index_col=None)
+        data.to_csv("sourcev.csv", index=None)
+        st.dataframe(data)
+        
+if choose == 'EDA':
+    if st.button('Perform EDA'):
+        st.header("perform profiling on Dataset")
+        profile_report = data.profile_report()
+        st_profile_report(profile_report)
+    
+if choose == 'Training':
+    st.header('Start Training your model now.')
+    choice = st.sidebar.selectbox("Select your Technique", ["Regression","Classification"])
+    target = st.selectbox('Select your Target Variable',data.columns)
+    if choice == 'Classification':
+        if st.sidebar.button('Train'):
+            s1 = ClassificationExperiment()
+            s1.setup(data=data, target=target)
+            setup_data = s1.pull()
+            st.info('The setup data is as follows:-')
+            st.table(setup_data)
+            
+            best_model1 = s1.compare_models()
+            compare_model = s1.pull()
+            st.info("The comparison of models is as follows:")
+            st.table(compare_model)
+            
+            best_model1
+            s1.save_model(best_model1,"Machine learning Model")
+    if choice == 'Regression':
+        if st.sidebar.button('Train'):
+            s2 = RegressionExperiment()
+            s2.setup(data=data, target=target)
+            setup_data = s2.pull()
+            st.info('The setup data is as follows:-')
+            st.table(setup_data)
+            
+            best_model2 = s2.compare_models()
+            compare_model = s2.pull()
+            st.info("The comparison of models is as follows:")
+            st.table(compare_model)
+            
+            best_model2
+            s2.save_model(best_model2,"Machine learning Model")
+
+if choose =="Download":
+    with open("Machine Learning model.pkl",'rb') as f:
+        st.caption("Download your model from here:")
+        st.download_button("Download the file",f,"Machine Learning model.pkl")
+            
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
+    
+
+    
+
